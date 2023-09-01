e2a14ed534fb0f51102d2f3757cf1fcb670a08ba
Fardin2112 <fardinalam2112@gmail.com>
2023-09-01 23:34:24 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,31 @@
-const express = require('express')
-require('dotenv').config()
+require("dotenv").config();
+
+const express = require("express");
+const mongoose = require("mongoose");
+const workourRoutes = require("./routes/workouts");
 
 //express app
-const app = express()
+const app = express();
 
 //middleware
-app.use((req,res,next)=>{
-    console.log(req.path,req.method)
-    next()
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log(req.path, req.method)
+  next()
 })
 
 //routes
-app.get('/',(req,res) => {
-    res.json({mssg:'Welcome to the app'})
-})
+app.use("/api/workouts", workourRoutes)
 
-//listen for request
-app.listen(process.env.PORT,() => {
-    console.log("listening on port",process.env.PORT)
-})
-
+//connect to db
+mongoose.connect(process.env.MONG_URI)
+  .then(() => {
+    //listen for request
+    app.listen(process.env.PORT, () => {
+      console.log(" connected to db and listening on port", process.env.PORT);
+    })
+  })
+  .catch((error) => {
+    console.log(error)
+  })
